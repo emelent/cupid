bb8157e4fb6058e9b5278af19cb9a7fb35779859
Merrick Kavolsky <technicmk@gmail.com>
2017-03-26 22:08:12 +0200

    Create block player state

---
+++ dragonslayer/player_states/block.lua
@@ -0,0 +1,47 @@
+local block = {
+	next_state = 'idle',
+	next_args = {}
+}
+
+local player
+local fsm
+local direction = 0
+
+function block.load(...)
+	local params = ...
+	player = params.player
+	fsm = params.player.smAction
+end
+
+function block.enter(prev_state, ...)
+	local params = ...
+  player.direction = 0
+	player.sprite:switch('block')
+end
+
+function block.exit()
+	direction = 0
+	block.next_args = {}
+	block.next_state = 'idle'
+end
+
+function block.update(dt)
+  if player.damageDealt then
+    player.sprite:switch('block')
+  end
+end
+
+function block.keyreleased(key, code)
+  --stop blocking before you damage delt
+	if  key == 'up' and not player.damageDealt then
+    if love.keyboard.isDown('a') then
+      fsm:setState('walk', {direction=-1})
+    elseif love.keyboard.isDown('d') then
+      fsm:setState('walk', {direction=1})
+    else
+      fsm:setState('idle')
+    end
+	end
+end
+
+return block
