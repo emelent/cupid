b47f9cabe410e04b6cfb873a47ac671f76b8234a
Merrick Kavolsky <technicmk@gmail.com>
2017-03-26 21:56:12 +0200

    Add block and dash animations

--- dragonslayer/player.lua
+++ dragonslayer/player.lua
@@ -1,6 +1,7 @@
 local player = {
   image = nil,
   direction = 0,
+  damageDealt = false,
   speed = 150,
   state = 'idle',
   smAction = state_machine.newFSM(),
@@ -12,13 +13,6 @@ local player = {
     x = 3,
     y = 3 
   },
-  -- keys = {
-  --   left = ret('a', 'left'),
-  --   right = ret('d', 'right'),
-  --   down = ret('s', 'down'),
-  --   up = ret('w', 'up'),
-  --   interact = ret('e')
-  -- }
 }
 
 function player.load()
@@ -34,11 +28,14 @@ function player.load()
     return player.position.x, player.position.y 
   end)
 
-  local endOfSlash = function()
-    player.smAction:setState(
-      player.smAction.current_state.next_state, 
-      player.smAction.current_state.next_args
-    )
+  local switchToNextState = function()
+    --if there is a next state switch to it
+    if player.smAction.current_state.next_state then
+      player.smAction:setState(
+        player.smAction.current_state.next_state, 
+        player.smAction.current_state.next_args
+      )
+    end
   end
   --Add animations
   sprite:addAnimation('idle', {
@@ -62,7 +59,7 @@ function player.load()
     frameWidth = 48,
     frameHeight = 48,
     stopAtEnd    = true,
-    onReachedEnd = endOfSlash,
+    onReachedEnd = switchToNextState,
     frames = {
       {1, 3, 6, 3, .08}
     }
@@ -72,7 +69,7 @@ function player.load()
     frameWidth = 48,
     frameHeight = 48,
     stopAtEnd = true,
-    onReachedEnd = endOfSlash,
+    onReachedEnd = switchToNextState,
     frames = {
       {4,3,6,3, 0.1}
     }
@@ -82,7 +79,7 @@ function player.load()
     frameWidth = 48,
     frameHeight = 48,
     stopAtEnd    = true,
-    onReachedEnd = endOfSlash,
+    onReachedEnd = switchToNextState,
     frames = {
       {1, 4, 6, 4, .08}
     }
@@ -92,21 +89,56 @@ function player.load()
     frameWidth = 48,
     frameHeight = 48,
     stopAtEnd = true,
-    onReachedEnd = endOfSlash,
+    onReachedEnd = switchToNextState,
     frames = {
       {4,4,6,4, 0.1}
     }
   })
-
+  sprite:addAnimation('dash', {
+    image = image,
+    frameWidth = 48,
+    frameHeight = 48,
+    stopAtEnd = true,
+    onReachedEnd = switchToNextState,
+    frames = {
+      {1,5,6,5, 0.05}
+    }
+  })
+  sprite:addAnimation('block', {
+    image = image,
+    frameWidth = 48,
+    frameHeight = 48,
+    stopAtEnd = true,
+    frames = {
+      {1,6,3,6, 0.1}
+    }
+  })
+  sprite:addAnimation('block_damage', {
+    image = image,
+    frameWidth = 48,
+    frameHeight = 48,
+    stopAtEnd = true,
+    onReachedEnd = function() 
+      --go to idle after blocking damage
+      player.smAction:setState('idle')
+      player.damageDealt = false
+    end,
+    frames = {
+      {3,6,6,6, 0.1}
+    }
+  })
 --set image scale
   sprite.sx = player.scale.x
   sprite.sy = player.scale.y
   player.sprite = sprite
+
 --load states
   local states_dir = 'player_states/'
   player.smAction:loadState('idle', require(states_dir .. 'idle'), {player = player})
   player.smAction:loadState('walk', require(states_dir .. 'walk'), {player = player})
   player.smAction:loadState('slash', require(states_dir .. 'slash'), {player = player})
+  player.smAction:loadState('block', require(states_dir .. 'block'), {player = player})
+
   -- start off in idle state
   player.smAction:setState('idle')
 end
